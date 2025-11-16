0837c33384623eab50972e289a7632934ed9e65a
TommyViriyaIrawan <167950147+TommyViriyaIrawan@users.noreply.github.com>
2025-11-16 18:52:29 +0700

    Commit No. 3: Integrate Room Database for Data Persistence

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("org.jetbrains.kotlin.kapt") // wajib untuk ROOM
 }
 
 android {
@@ -26,10 +27,12 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
     kotlinOptions {
         jvmTarget = "11"
     }
@@ -37,15 +40,22 @@ android {
 
 dependencies {
 
+    // existing
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
-    implementation ("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
 
+    // ROOM DATABASE
+    val roomVersion = "2.6.0"
+
+    implementation("androidx.room:room-runtime:$roomVersion")
+    kapt("androidx.room:room-compiler:$roomVersion")
 }
--- app/src/main/java/com/example/lab_week_10/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_10/MainActivity.kt
@@ -1,17 +1,21 @@
 package com.example.lab_week_10
 
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
-import com.example.lab_week_10.R
+import androidx.room.Room
+import android.widget.Button
+import android.widget.TextView
+import com.example.lab_week_10.database.Total
+import com.example.lab_week_10.database.TotalDatabase
 import com.example.lab_week_10.viewmodels.TotalViewModel
-
 
 class MainActivity : AppCompatActivity() {
 
-    // ViewModel
+    // Room Database Instance
+    private val db by lazy { prepareDatabase() }
+
+    // ViewModel Instance
     private val viewModel by lazy {
         ViewModelProvider(this)[TotalViewModel::class.java]
     }
@@ -20,7 +24,28 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        initializeValueFromDatabase()
         prepareViewModel()
+    }
+
+    // Build database
+    private fun prepareDatabase(): TotalDatabase {
+        return Room.databaseBuilder(
+            applicationContext,
+            TotalDatabase::class.java,
+            "total-database"
+        ).allowMainThreadQueries()
+            .build()
+    }
+
+    // Read database
+    private fun initializeValueFromDatabase() {
+        val total = db.totalDao().getTotal(ID)
+        if (total.isEmpty()) {
+            db.totalDao().insert(Total(id = 1, total = 0))
+        } else {
+            viewModel.setTotal(total.first().total)
+        }
     }
 
     private fun updateText(total: Int) {
@@ -29,16 +54,21 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun prepareViewModel() {
-
-        // Observe the LiveData object
-        viewModel.total.observe(this, {
-            // Whenever the value changes, update text
-            updateText(it)
-        })
+        viewModel.total.observe(this) { newTotal ->
+            updateText(newTotal)
+        }
 
         findViewById<Button>(R.id.button_increment).setOnClickListener {
             viewModel.incrementTotal()
         }
     }
 
+    override fun onPause() {
+        super.onPause()
+        db.totalDao().update(Total(ID, viewModel.total.value ?: 0))
+    }
+
+    companion object {
+        const val ID: Long = 1
+    }
 }
